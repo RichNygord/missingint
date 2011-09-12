35459a691f3af2e7f73f08ca07595e8a0df53a26
Richard Nygord <rvnygord@yahoo.com>
2011-09-12 04:49:06 -0700

    First github

--- lib/missingint.rb
+++ lib/missingint.rb
@@ -1,31 +1,19 @@
-# Solution to the missing_int problem and some tests.
-# By Richard Nygord 9/8/2011
-# Test instructions/runs are in the final block comment.
+#missingint.rb
+# By Richard Nygord 
+# Created 9/8/2011
+# Modified 9/12/2011
+# Return the missing number in an array sized n containing numbers in range (1..n+1) in linear time with no additional size complexity.  Add missing_int to Array.
 
 module MissingInt
-
-  # Return the missing number in an array sized n containing numbers in range (1..n+1) in linear time with no additional size complexity.
-  # I left in the intermediate soultions to show my thought process.
 
   def self.mi ar
     return -1 if ar == []
     n=ar.length
     
-    #using new bit array
-    #b=Array.new(n+2,true) #start with all missing with room for 0 and n+1
-    #ar.each {|v| b[v]=false}  #record not missing ones
-    #b[0]=false
-    #b.index true          #return only the missing one
-    
-    #using signs in a new array
-    #m=[0]+ar+[n+1]
-    #ar.each {|v| m[v]=-m[v]}  #record not missing ones
-    #m.index {|v| v>0}     #return only the missing one
-    
-    #using signs in the existing array - linear + in place storage
+    #using sign-bit in the existing array - linear + in place storage
     ar.unshift 0
     ar.push n+1
-    (1..n).each {|i| ar[ar[i].abs] = -ar[ar[i].abs]}  #record not missing ones in sign-bit of index of value remebering that values must be asbed!
+    (1..n).each {|i| ar[ar[i].abs] = -ar[ar[i].abs]}  #record not missing ones in sign-bit of index of value remebering that values must be asb-ed!
     
     res = ar.index {|v| v>0} #missing one is left positive
     ar.shift
@@ -34,7 +22,6 @@ module MissingInt
     res                                      #da!
   end
 end
-
 
 class Array
   def missing_int
