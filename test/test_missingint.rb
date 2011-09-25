6239b14e669f06028d00c628928bb5114c09330d
Richard Nygord <rvnygord@yahoo.com>
2011-09-25 10:58:04 -0700

    Fixed rake test.

--- test/test_missingint.rb
+++ test/test_missingint.rb
@@ -6,10 +6,10 @@ class TestMissingInt < Test::Unit::TestCase
     assert_equal( 4, [2,1,3].missing_int )
     assert_equal( 1, [3,2].missing_int )
     assert_equal( 3, [1,2].missing_int )
-    assert_equal( -1, [].missing_int )
+    ex = assert_raise( RuntimeError ) { [].missing_int }
+    assert_match( /no missing integers for empty Array/, ex.message )
     ex = assert_raise( NoMethodError ) { 2.missing_int }
     assert_match( /undefined method `missing_int'/, ex.message )
   end
-  
 end
 
