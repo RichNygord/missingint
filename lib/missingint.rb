e6b4161e580270ea3444099abe33e2d302b710c1
Richard Nygord <rvnygord@yahoo.com>
2011-09-25 10:20:34 -0700

    Cleanup with just arithmetic algorithm.

--- lib/missingint.rb
+++ lib/missingint.rb
@@ -2,38 +2,20 @@
 # By Richard Nygord 
 # Created 9/8/2011
 # Modified 9/25/2011 to add the arithmetic version using: 1+ 2+ ... + n = n(n+1)/2
-# Return the missing integer in an array sized n, containing integers in range (1..n+1) not ordered, in linear time with no additional size complexity.  Add missing_int to Array.
+# Return the missing integer in an array sized n, containing integers in range (1..n+1), not ordered, in linear time with no additional size complexity.  Add missing_int to Array.
 
 module MissingInt
 
-  # Using sign-bit in the existing array - linear + in place storage
-  # This version is only for pedagogical purposes, mi2 is used
-  def self.mi1 ar
-    raise RuntimeError, 'no mising integers for empty Array' if ar == []
-    n=ar.length
-    
-    ar.unshift 0
-    ar.push n+1
-    (1..n).each {|i| ar[ar[i].abs] = -ar[ar[i].abs]}  #record not missing ones in sign-bit of index of value remebering that values must be asb-ed!
-    
-    res = ar.index {|v| v>0} #missing one is left positive
-    ar.shift
-    ar.pop
-    ar.length.times {|i| ar[i] = ar[i].abs}  #ta...
-    res                                      #da!
-  end
-
-  # Here is a *much* better one using this: 1+ 2+ ... + n = n(n+1)/2
-  def self.mi2 ar
+  def self.mi ar
     raise RuntimeError, 'no missing integers for empty Array' if ar == []
-    (ar.length+1) * (ar.length+2) / 2 - ar.inject(:+)
+    (ar.length+1) * (ar.length+2) / 2 - ar.inject(:+)  # 1+ 2+ ... + n = n(n+1)/2
   end
   
 end
 
 class Array
   def missing_int
-    MissingInt.mi2 self
+    MissingInt.mi self
   end
 end
 
