8a8719f7b8b91f617399752e45a704061d280c29
Richard Nygord <rvnygord@yahoo.com>
2011-09-25 11:31:48 -0700

    Removed the module.

--- lib/missingint.rb
+++ lib/missingint.rb
@@ -4,18 +4,10 @@
 # Modified 9/25/2011 to add the arithmetic version using: 1+ 2+ ... + n = n(n+1)/2
 # Return the missing integer in an array sized n, containing integers in range (1..n+1), not ordered, in linear time with no additional size complexity.  Add missing_int to Array.
 
-module MissingInt
-
-  def self.mi ar
-    raise RuntimeError, 'no missing integers for empty Array' if ar == []
-    (ar.length+1) * (ar.length+2) / 2 - ar.inject(:+)  # 1+ 2+ ... + n = n(n+1)/2
-  end
-  
-end
-
 class Array
   def missing_int
-    MissingInt.mi self
+    raise RuntimeError, 'no missing integers for empty Array' if self == []
+    (self.length+1) * (self.length+2) / 2 - self.inject(:+)  # 1+ 2+ ... + n = n(n+1)/2
   end
 end
 
