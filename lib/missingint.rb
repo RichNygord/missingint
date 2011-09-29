2ecaf9ee8e27e4a71e71f92de991758be28efe62
Richard Nygord <rvnygord@yahoo.com>
2011-09-28 21:36:44 -0700

    Added C version.

--- lib/missingint.rb
+++ lib/missingint.rb
@@ -2,6 +2,7 @@
 # By Richard Nygord 
 # Created 9/8/2011
 # Modified 9/25/2011 to add the arithmetic version using: 1+ 2+ ... + n = n(n+1)/2
+# Added C version 9/28/2011
 # Return the missing integer in an array sized n, containing integers in range (1..n+1), not ordered, in linear time with no additional size complexity.  Add missing_int to Array.
 
 class Array
@@ -10,3 +11,16 @@ class Array
   end
 end
 
+
+=begin
+
+And, just for the hell of it, here is my C version:
+
+int missing_int(int n, int* ints)
+{
+  int result = (n + 1) * (n + 2) / 2;
+  while (n--) result -= *ints++;
+  return result;
+}
+
+=end
